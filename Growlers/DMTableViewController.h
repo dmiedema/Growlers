12880ff72d81adb3c62135670d945664ec24c3b7
Daniel Miedema <danielmiedema@me.com>
2013-07-17 01:28:42 -0700

    Created UITableView, currently it performs network request for debugging purposes.

---
+++ Growlers/DMTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMTableViewController.h
+//  Growlers
+//
+//  Created by Daniel Miedema on 7/17/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMTableViewController : UITableViewController
+
+@end
