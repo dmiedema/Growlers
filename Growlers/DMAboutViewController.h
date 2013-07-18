43388ea3c0d0571c22b0739c59b6c6af3638a895
Daniel Miedema <danielmiedema@me.com>
2013-07-17 22:55:20 -0700

    Created AboutViewController. it doesn’t do anything yet.

---
+++ Growlers/DMAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMAboutViewController.h
+//  Growlers
+//
+//  Created by Daniel Miedema on 7/17/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAboutViewController : UIViewController
+
+@end
