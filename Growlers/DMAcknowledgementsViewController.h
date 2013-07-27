5cf12df025f3b8a26008a6ea23fe5e7944883700
Daniel Miedema <danielmiedema@me.com>
2013-07-27 01:52:55 -0700

    Added Acknowledgements view controller. and all that magic that comes along with it.

---
+++ Growlers/DMAcknowledgementsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMAcknowledgementsViewController.h
+//  Growlers
+//
+//  Created by Daniel Miedema on 7/27/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAcknowledgementsViewController : UIViewController
+@property (nonatomic, strong) IBOutlet UIWebView *webview;
+@end
