124cd63452e9e5fe44f536a3049a01d648772b96
Daniel Miedema <danielmiedema@me.com>
2013-07-29 23:39:11 -0700

    setup database.js Will need to be tested though

--- database.js
+++ database.js
@@ -1,11 +1,35 @@
-var db = require('mongodb');
+var db = require('mongoose');
+db.connect('mongodb://localhost/GrowlMovement');
+
+var favoriteSchema = db.Schema({
+  beer: {name: String, brewery: String},
+  favorites: Array
+});
+
+var Favorite = db.model('Favorite', favoriteSchema);
 
 module.exports = {
-  find: function(favorite) {
-
+  find: function(fav) {
+    return Favorite.findOne({'beer.name': fav.beername, 'beer.brewery': fav.brewery}).count() == true;
   }
-  insert: function(favorite) {
+  insert: function(fav, createEntry) {
+    var create;
+    if (typeof(createEntry) === 'undefined')
+      create = true;
+    else create = createEntry;
 
+    if(create) {
+      var beer = new Favorite({beer: {name: fav.beername, brewery: fav.brewery}, favorites: [fav.uniqueID]});
+      beer.save(function(error, favorite) {
+        if (error) {
+          // error, handle it.
+        }
+      });
+    } else {
+      var found = Favorite.findOne({'beer.name': fav.beername, 'beer.brewery': fav.brewery});
+      found.favorites.push(fav.uniqueID);
+      Favorite.save(found);
+    }
   }
 };
 
