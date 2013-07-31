3d6ace1d556b282e4b011c76392eeeae55266ce8
Daniel Miedema <danielmiedema@me.com>
2013-07-30 23:32:11 -0700

    (hopefully) this will make the view round and look like the mail indicator.

--- Growlers/DMGrowlerTableViewCell.m
+++ Growlers/DMGrowlerTableViewCell.m
@@ -26,4 +26,9 @@
     // Configure the view for the selected state
 }
 
+- (void)viewDidLayoutSubviews {
+    _favoriteMarker.layer.masksToBounds = YES;
+    _favoriteMarker.layer.cornerRadius = _favoriteMarker.bounds.size.width / 2.0;
+}
+
 @end
