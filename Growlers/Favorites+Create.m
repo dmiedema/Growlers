086294010fb35e4b135e3683e39636d3fffa67ec
Daniel Miedema <danielmiedema@me.com>
2013-08-05 23:53:45 -0700

    Entity name was wrong. Spelling is hard.

--- Growlers/Favorites+Create.m
+++ Growlers/Favorites+Create.m
@@ -13,7 +13,12 @@
 + (Favorites *)favoriteWithInfo:(NSDictionary *)info inManagedObjectContext:(NSManagedObjectContext *)context {
     Favorites *favorite = nil;
     
-    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Favorite"];
+    NSLog(@"Favorites+Create");
+    
+    NSLog(@"Info: %@", info);
+    NSLog(@"Context: %@", context);
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Favorites"];
     request.predicate = [NSPredicate predicateWithFormat:@"name = %@ and brewer = %@", info[@"name"], info[@"brewer"]];
     NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
     request.sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
@@ -21,15 +26,20 @@
     NSError *error = nil;
     NSArray *matches = [context executeFetchRequest:request error:&error];
     
+    NSLog(@"Matches: %@", matches);
+    
     if (!matches || matches.count > 1) {
         // error
     } else if (matches.count == 0) {
-        favorite = [NSEntityDescription insertNewObjectForEntityForName:@"Favorite" inManagedObjectContext:context];
+        NSLog(@"Creating Entry...");
+        favorite = [NSEntityDescription insertNewObjectForEntityForName:@"Favorites" inManagedObjectContext:context];
         favorite.name = info[@"name"];
         favorite.brewer = info[@"brewer"];
     } else {
         favorite = [matches lastObject];
     }
+    
+    NSLog(@"Entry Created! - %@", favorite);
     
     return favorite;
 }
