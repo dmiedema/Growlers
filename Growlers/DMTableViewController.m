1bf3021bbdcc6eb38f2818a7eb0ce523625ee0f3
Daniel Miedema <danielmiedema@me.com>
2013-09-12 22:27:58 -0700

    Try my best to make the text fit.

--- Growlers/DMTableViewController.m
+++ Growlers/DMTableViewController.m
@@ -344,6 +344,17 @@ BOOL _performSegmentChange;
     static NSString *CellIdentifier = @"growlerCell";
     DMGrowlerTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
 
+    // try my best to make everything fit.
+    cell.beerName.adjustsLetterSpacingToFitWidth = YES;
+    cell.beerName.adjustsFontSizeToFitWidth = YES;
+    
+    cell.brewery.adjustsLetterSpacingToFitWidth = YES;
+    cell.brewery.adjustsFontSizeToFitWidth = YES;
+    
+    cell.beerInfo.adjustsLetterSpacingToFitWidth = YES;
+    cell.beerInfo.adjustsFontSizeToFitWidth = YES;
+    
+    
     NSDictionary *beer;
     if (tableView == self.searchDisplayController.searchResultsTableView) {
         beer = self.filteredBeers[indexPath.row];
