7cb1f0d34fb0021207a6befe46dead4620ed32da
Daniel Miedema <danielmiedema@me.com>
2013-09-17 22:59:49 -0700

    Fixing precent escapes and also making sure beer is not a favorite when attempting to favorite

--- Growlers/DMAppDelegate.m
+++ Growlers/DMAppDelegate.m
@@ -122,35 +122,19 @@
         NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithCapacity:queryParameters.count];
         for (NSString *param in queryParameters) {
             NSArray *args = [param componentsSeparatedByString:@"="];
-            [parameters setValue:args[1] forKey:args[0]];
+            [parameters setValue:[args[1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding] forKey:args[0]];
         }
         
         // make sure beer & brewer are set
         if([parameters.allKeys containsObject:@"beer"] && [parameters.allKeys containsObject:@"brewer"]) {
             DMCoreDataMethods *coreData = [[DMCoreDataMethods alloc] initWithManagedObjectContext:self.managedObjectContext];
             [parameters setValue:[[NSUserDefaults standardUserDefaults] stringForKey:kGrowler_Last_Selected_Store] forKey:@"store"];
-            [coreData favoriteBeer:parameters];
+            if(![coreData isBeerFavorited:parameters])
+                [coreData favoriteBeer:parameters];
             return YES;
         }
     }
     return NO;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
