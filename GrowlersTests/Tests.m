66e0504a0f7713432406f9f830e8190935671506
Daniel Miedema <danielmiedema@me.com>
2013-11-12 18:21:07 -0800

    Added Tests.m

---
+++ GrowlersTests/Tests.m
@@ -0,0 +1,34 @@
+//
+//  Tests.m
+//  Growlers
+//
+//  Created by Daniel Miedema on 11/9/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
