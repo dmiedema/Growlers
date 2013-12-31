a56abd05d1468982478f541ecd772455a154b7e8
Daniel Miedema <danielmiedema@me.com>
2013-12-30 23:09:52 -0800

    Change it so that it deleted all beers found

--- Growlers/DMCoreDataMethods.m
+++ Growlers/DMCoreDataMethods.m
@@ -105,7 +105,10 @@
     NSError *error = nil;
     NSArray *matches = [self.managedContext executeFetchRequest:request error:&error];
     
-    [self.managedContext deleteObject:matches.lastObject];
+    for (id beer in matches) {
+        [self.managedContext deleteObject:beer];
+    }
+    
     if (![self.managedContext save:&error]){
         // handle error
     }
